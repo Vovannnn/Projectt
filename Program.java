13c8a63c7c356276d416c7dfee083ee10114df32
Vovannnn <vovan123123@gmail.com>
2017-01-16 08:35:06 +0200

    SUM

--- Program.java
+++ Program.java
@@ -1,7 +1,12 @@
 public class Program
 {
-	public static void main(String [] args);
+	public static void main(String [] args)
 	{
 		System.out.println("HELLO");
 	}
+	public int sum(int a, int b)
+	{
+		return a + b;
+	}
+		
 }
