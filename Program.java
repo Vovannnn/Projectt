5a488368317832152e03ec04734d2a1e1a4253f1
Vovannnn <vovan123123@gmail.com>
2017-01-16 11:05:39 +0200

    Start

--- Program.java
+++ Program.java
@@ -5,15 +5,23 @@ public class Program
 	public static void main(String [] args)
 	{
 		System.out.println("HELLO");
+		Program program = new Program();
+		program.start();
 	}
 	public int sum(int a, int b)
 	{
 		return a + b;
 	}
+	public void start()
+	{
+		int a = input("first number");
+		int b = input("seccont number");
+		System.out.println("Sum of numbers" + sum(a,b));
+	}
 	public int input(String text)
 	{
 		System.out.println(text);
-		return scanner.nextInt();
+		return s.nextInt();
 	}
 		
 }
